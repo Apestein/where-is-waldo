996abc82431d3736009406663c9d78489616f8a4
annony <ltn2057@protonmail.com>
2022-11-28 13:25:49 -0500

    add background-img

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import thisStyle from "./styles/App.module.css"
+import bgImg from "./assets/universe-113.jpg"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <header></header>
+      <main>
+        <img className={thisStyle.bg} src={bgImg} alt="bg-img" />
+      </main>
+      <footer></footer>
+    </>
+  )
 }
 
-export default App;
+export default App
